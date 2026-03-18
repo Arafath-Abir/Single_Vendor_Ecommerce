036b443923481c9e4c989e16748da1fa00c4406e
Arafath Abir <arafathabir07@gmail.com>
2026-03-19 03:35:32 +0600

    remove generated files

--- Elanzo/settings.py
+++ Elanzo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2odr*iracppiviz=s(-g1cz)h*^5!_6@86wo3*!#1kde$l*ng2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- staticfiles/javascript/base.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const messages = document.querySelectorAll('.alert');
-    messages.forEach(msg => {
-        setTimeout(() => {
-            msg.style.display = 'none';
-        }, 3000);
-    });
-    console.log("Static JS Loaded!");
-});
